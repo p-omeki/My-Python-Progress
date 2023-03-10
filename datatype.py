735b5abf2f2480bdf6b51118ee5d30912ce766cf
unknown <partymeki@gmail.com>
2023-03-10 14:22:53 +0300

    Update Data Types

--- datatype.py
+++ datatype.py
@@ -1,4 +1,5 @@
-#Python Range
+#Python Range - Is na inbuilt-function in python which return a sequence of numbers from 0
+## with an increanent of 1 until it reaches a specified sequence
 '''Works like a for loop with integer value declared, initialization and inc/dec ment
   denoted as "range()"
   The original format is range(start, stop, step).
