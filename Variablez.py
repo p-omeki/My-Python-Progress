c871384021edd0a96ec8150b0de11053c7a3d8c3
unknown <partymeki@gmail.com>
2023-03-10 11:47:59 +0300

    Variables in Python

---
+++ Variablez.py
@@ -0,0 +1,10 @@
+## Variables in a python are memory location used to store values in a python program
+## Variables are first created then values are assigned to them.
+## Creating a variable below;
+name = "Patrick Meki" #Creating a string variable
+year = 3 #Creating  an integer variable
+study = 3.2 #Creating  a floating point value variable
+
+print (name)
+
+del (study) # Now, what this will do is that it will delete variable 'study' and if we try to print variable study it will bring an error!
