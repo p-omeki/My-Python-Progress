5ffde67ee345e8a5fa4f120d7d3f7eec77c64d54
unknown <partymeki@gmail.com>
2023-03-10 12:51:59 +0300

    Updated Variablez

--- Variablez.py
+++ Variablez.py
@@ -8,3 +8,22 @@ study = 3.2 #Creating  a floating point value variable
 print (name)
 
 del (study) # Now, what this will do is that it will delete variable 'study' and if we try to print variable study it will bring an error!
+
+##Local variable and Global variable
+'''
+The major difference in this is that in local variable, variables cannot be accessed outside a function
+while in Global variable, variable can be accessed outside of a function. Below, it's a simple
+Local and Global Variable difference
+'''
+#Local Variable
+'''def sum(x,y):
+    sum =x+y
+    return sum
+print (sum(5,10))'''
+#Global Variable
+x = 5
+y = 10
+def sum():
+    sum = x+y
+    return sum
+print(sum()) 
