a6413cb44023e8ee07536bf89c92a8528b9ce62c
unknown <partymeki@gmail.com>
2023-04-28 18:51:02 +0300

    Updated Trial-Repo
    The update is only avalable on the tribranch because  we had switched to it earlier. We ain't working on the main branch anymore

--- trial.py
+++ trial.py
@@ -2,3 +2,10 @@ if True:
     print ("True")
 else:
     print ("False")
+    
+    ## This is only applicable to tribranch 
+    '''
+    The Tribranch is the feature branch of the main branch
+    Now the code has two branches; tribranch and main branches
+    
+    '''
