2d3a38e32176d9cac896b028484961ce60d5109d
unknown <partymeki@gmail.com>
2023-05-01 00:39:54 +0300

    Updated Trial-Repo

--- trial.py
+++ trial.py
@@ -1,9 +1,8 @@
-'''
+
 if True:
     print ("True!")
 else:
     print ("False")
-    '''
     
     ## This is only applicable to tribranch 
     '''
@@ -18,8 +17,25 @@ else:
     Statements between {},[] and () however don't need the character (\) to continue on to the next line
     The below statement works perfectly in Python..
     '''
-    '''
+    
     days =['Monday','Tuesday','Wednesday',
             'Thursday', 'Friday']
             
+            
+            ##Quotation and Comments in Python
             '''
+            In python, there are 3 different types of quotation. Single (''), Double ("") and Triple(''' ''') quotes
+            Check the examples below on quoatation in Python:
+            '''
+            word = 'Word'
+            statement = "This is a statement"
+            paragraph = '''This quote is used mostly in multi-line statements
+            and paragraph blocks in python
+            '''
+            ##Comments
+            #This statements is an example of a comments and it makes python codes to be easily readable to
+            #humans. It also make it easy to understand code. Check example below:
+            
+            #First comment
+            print ("Hello World") #Second comment
+            
