0e02de3dff5c3a3dd9f3b4e10a1e67bfb7c66b18
unknown <partymeki@gmail.com>
2023-05-01 00:17:14 +0300

    Updated Trial
    Today did one code before sleep

--- trial.py
+++ trial.py
@@ -1,7 +1,9 @@
+'''
 if True:
     print ("True!")
 else:
     print ("False")
+    '''
     
     ## This is only applicable to tribranch 
     '''
@@ -9,3 +11,15 @@ else:
     Now the code has two branches; tribranch and main branches
     
     '''
+    ##Python multi-line continuation character
+    '''
+    Statements in python typically ends with a new line. However, python allows the use of line continuation character (\)
+    that allows the line to continue.
+    Statements between {},[] and () however don't need the character (\) to continue on to the next line
+    The below statement works perfectly in Python..
+    '''
+    '''
+    days =['Monday','Tuesday','Wednesday',
+            'Thursday', 'Friday']
+            
+            '''
