fc4598ecdfa8f77004574813b34e92d065ba31fe
unknown <partymeki@gmail.com>
2023-05-01 17:32:27 +0300

    Updated Notes and Trial Repo

--- notes.py
+++ notes.py
@@ -1,29 +1,29 @@
    
-    ## This is only applicable to tribranch 
-    '''
-    The Tribranch is the feature branch of the main branch
+ ## This is only applicable to tribranch 
+ '''
+The Tribranch is the feature branch of the main branch
     Now the code has two branches; tribranch and main branches
     '''
     ##Python multi-line continuation character
-    '''
+ '''
     Statements in python typically ends with a new line. However, python allows the use of line continuation character (\)
     that allows the line to continue.
     Statements between {},[] and () however don't need the character (\) to continue on to the next line
     The below statement works perfectly in Python..
     '''
     
-    days =['Monday','Tuesday','Wednesday',
+ days =['Monday','Tuesday','Wednesday',
             'Thursday', 'Friday']
             
             
     ##Quotation and Comments in Python
-    '''
+ '''
     In python, there are 3 different types of quotation. Single (''), Double ("") and Triple(''' ''') quotes
     Check the examples below on quoatation in Python:
     '''
-    word = 'Word'
-    statement = "This is a statement"
-    paragraph = '''This quote is used mostly in multi-line statements
+ word = 'Word'
+ statement = "This is a statement"
+ paragraph = '''This quote is used mostly in multi-line statements
             and paragraph blocks in python
             '''
     ##Comments
@@ -31,7 +31,12 @@
     #humans. It also make it easy to understand code. Check example below:
             
     #First comment
-    print ("Hello World") #Second comment
+ print ("Hello World") #Second comment
+ #Python tuple and lists are similar, the only difference between the two is that 
+ #~list are enclosed with brackets [] and it can be changed (elements) while tuple are 
+ #~enclosed with parenthesis ()  and cannot be changed. Basically, tuples are 
+ #~read-only lists.
+ #The [] and () enclosers are only in the output.
             
             
             
--- trial.py
+++ trial.py
@@ -1,4 +1,21 @@
+'''
 if True:
     print ("True!")
 else:
     print ("False")
+    '''
+'''  
+counter = 100
+print (counter)
+    
+del counter
+print (counter)
+'''
+'''
+#Python String data types
+string = ("Hello World!")
+print (string)
+print (string[0])
+print (string [2:5])
+print (string[2:])
+'''
