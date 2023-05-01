596e94172b704de03ec6fe78ac58df2f86a78a93
unknown <partymeki@gmail.com>
2023-05-01 10:16:50 +0300

    Updates

--- notes.py
+++ notes.py
@@ -1,9 +1,4 @@
-
-if True:
-    print ("True!")
-else:
-    print ("False")
-    
+   
     ## This is only applicable to tribranch 
     '''
     The Tribranch is the feature branch of the main branch
