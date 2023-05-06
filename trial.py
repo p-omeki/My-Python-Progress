983e6667eddb9f8d96bd3671f4654aa819203c63
unknown <partymeki@gmail.com>
2023-05-06 17:54:15 +0300

    Updated Trial Repo and there's a Question

--- trial.py
+++ trial.py
@@ -19,6 +19,27 @@ print (string[0])
 print (string [2:5])
 print (string[2:])
 '''
-a= 70
-if (a>60):
- print ("Old as fuck")
+'''
+a= 40
+if a>60:
+    print("Old as fuck")
+elif a<10:
+    print("Baby")
+else:
+    if a==20:
+        print ("3 years down")
+    elif a!=20:
+        print ("Younger than 18")
+    else:
+        print ("Young and Dumb")
+        print("Lost somewhere")
+    '''
+#Accessing values in a string
+#Quiz: Am trying to run this simple code from https://www.tutorialspoint.com/python/python_strings.htm and I can't get deeper understanding of it.
+#This is the code, anyone with help can chip in. Below is the code:
+var1 = "Hello World"
+var2 = "Python Programming"
+
+print , var1[0]
+print , var2[1:5]
+#When I run the code above, it gives no output. What might be wrong?
